02e74aa6e65b53f371d3669b6af744272d4c1db6
shahmojno <mojno12@gmail.com>
2022-03-01 00:10:24 +0600

    3 commit

--- api.js
+++ api.js
@@ -1,8 +1,45 @@
-const loadPhone = searchPhone => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`;
+const loadPhone = () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayPhone(data));
 }
 
 loadPhone();
+
+
+
+const displayPhone = phones => {
+    console.log(phones.data);
+    const container = document.getElementById('phones');
+    const phone = phones.data;
+    phone.forEach(phone2 => {
+        // console.log(container);
+        const div = document.createElement('dive');
+        div.innerHTML = `
+        <h1> ${phone2.brand}</h1>
+        <img src="${phone2.image}" class="card-img-top" alt="...">
+        `;
+
+        container.appendChild(div);
+
+
+    }
+
+    );
+
+
+
+    // const container = document.getElementById('phones');
+    // data.forEach(phone => {
+    //     console.log(phone);
+    //     const div = document.createElement('div');
+    //     div.innerHTML = `
+    // <h1> ${phone.brand}</h1>
+    // `;
+    //     container.appendChild(div);
+    // })
+
+}
+
+// displayPhone();
