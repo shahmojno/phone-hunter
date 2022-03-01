7eca9c9010b913ff4888a5fdb3995961abb7954a
shahmojno <mojno12@gmail.com>
2022-03-01 23:52:35 +0600

    4 commit

--- api.js
+++ api.js
@@ -1,5 +1,18 @@
-const loadPhone = () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search`;
+// display show hide
+const showHideSearchResult = displayStyle => {
+    document.getElementById('block').style.display = displayStyle;
+}
+
+const showHideSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
+
+
+
+
+const loadPhone = searchPhone => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data));
@@ -8,38 +21,42 @@ const loadPhone = () => {
 loadPhone();
 
 
+const searchPhone = () => {
+    const searchText = document.getElementById('search-field').value;
+    showHideSpinner('block');
+    showHideSearchResult('none');
+    loadPhone(searchText);
+    document.getElementById('search-field').value = '';
+
+}
+
+
 
 const displayPhone = phones => {
-    console.log(phones.data);
+    // console.log(phones.data);
     const container = document.getElementById('phones');
     const phone = phones.data;
+    console.log(phone);
+    container.textContent = '';
+    // console.log(phone);
     phone.forEach(phone2 => {
-        // console.log(container);
+        console.log(container);
         const div = document.createElement('dive');
         div.innerHTML = `
-        <h1> ${phone2.brand}</h1>
+        <h5> Brand : ${phone2.brand}</h5>
+        <p> Name :  ${phone2.phone_name}</p>
         <img src="${phone2.image}" class="card-img-top" alt="...">
+        <br>
+        <button class = " mx-4 my-4">Details</button>
         `;
 
         container.appendChild(div);
 
 
-    }
-
-    );
-
-
-
-    // const container = document.getElementById('phones');
-    // data.forEach(phone => {
-    //     console.log(phone);
-    //     const div = document.createElement('div');
-    //     div.innerHTML = `
-    // <h1> ${phone.brand}</h1>
-    // `;
-    //     container.appendChild(div);
-    // })
-
+    });
+    showHideSpinner('none');
+    showHideSearchResult('block');
 }
 
 // displayPhone();
+
